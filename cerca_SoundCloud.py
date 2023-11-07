6f4c38c34dbbe8ac55ff87a5944bb09d2b1ad927
VanePrieto80 <150071434+VanePrieto80@users.noreply.github.com>
2023-11-07 16:04:20 +0100

    Add files via upload

---
+++ cerca_SoundCloud.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created 6-XI-2023
+
+@author: Sergi Benito Ivern
+         Vanesa Prieto Prieto
+"""
+
+
+CERCA = "top 100 songs 2023" # Clave per a realitzar la bùsqueda a SoundCloud
+N = 20 # Nombre de vegades que es realitzarà SCROLL per a descarregar les dades
+NOM_ARXIU = "/Top100Songs2023.csv" # Nom de l'arxiu on es descarregaran les dades.
